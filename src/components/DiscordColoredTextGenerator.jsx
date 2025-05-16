7783c938a6926325e62754832372bf0b9c691642
thebardofavon <152669825+thebardofavon@users.noreply.github.com>
2025-05-16 13:46:14 +0530

    Remove improvement section from About in DiscordColoredTextGenerator to streamline content

--- src/components/DiscordColoredTextGenerator.jsx
+++ src/components/DiscordColoredTextGenerator.jsx
@@ -260,8 +260,8 @@ const DiscordColoredTextGenerator = () => {
         <Title order={3} mt="md">About</Title>
         <Text>This is a simple app that creates colored Discord messages using the ANSI color codes available on the latest Discord desktop versions.</Text>
         <Text mt="xs">To use this, write your text, select parts of it and assign colors to them, then copy it using the button below, and send in a Discord message.</Text>
-        <Title order={3} mt="md">Improvement</Title>
-        <Text>The original website allowed text outside the text area to be selected and styled, which was identified as a flaw. This issue has been addressed in this implementation by ensuring that only text within the editor can be styled.</Text>
+        {/* <Title order={3} mt="md">Improvement</Title>
+        <Text>The original website allowed text outside the text area to be selected and styled, which was identified as a flaw. This issue has been addressed in this implementation by ensuring that only text within the editor can be styled.</Text> */}
         <Title order={3} mt="md">Source Code</Title>
         <Text>
           This app runs entirely in your browser and the source code is freely available on{' '}
